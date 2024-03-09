64cee06536715af26da080d1619d83538959d3f5
MUKILAN S <mukilan069@gmail.com>
2024-03-09 15:49:06 +0530

    Landing page and Auth Pages

--- routes/dashboardRoute.js
+++ routes/dashboardRoute.js
@@ -7,4 +7,9 @@ const pageController = require("../controllers/pageController")
 
 router.get("/",pageController.homePage)
 
+router.get("/login",pageController.loginPage)
+
+router.get("/register",pageController.registerPage)
+
+
 module.exports = router
